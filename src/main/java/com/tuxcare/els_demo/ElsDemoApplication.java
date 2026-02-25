0dc508d8c54afdf6208d75caa8479da59553f6bd
Toshiki Nishioka @ TuxCare <tnishioka@rx1000.local>
2026-02-25 14:26:30 +0900

    first commit

---
+++ src/main/java/com/tuxcare/els_demo/ElsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tuxcare.els_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsDemoApplication.class, args);
+	}
+
+}
